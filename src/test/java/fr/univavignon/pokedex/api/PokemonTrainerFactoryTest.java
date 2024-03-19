1bafa54acd59f1610ce77830533d3d6f23db4480
Quentin_jlard <60688034+Quentinjlard@users.noreply.github.com>
2024-03-19 16:51:22 +0100

    Ajout des Test

---
+++ src/test/java/fr/univavignon/pokedex/api/PokemonTrainerFactoryTest.java
@@ -0,0 +1,13 @@
+package fr.univavignon.pokedex.api;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.when;
+
+
+public class PokemonTrainerFactoryTest {
+
+}
